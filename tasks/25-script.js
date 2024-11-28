f1dc4587d4792e0e455f113e3ca1196c78750a50
Viktor Svertoka <victor.svertoka@gmail.com>
2024-11-28 17:41:25 +0200

    Create 25-script.js

---
+++ tasks/25-script.js
@@ -0,0 +1,11 @@
+let cities = ['London', 'New York', 'Mumbai'];
+
+console.log(cities);
+
+cities[cities.length - 1] = 'Mexico City';
+
+console.log(cities);
+
+// Now you are ready to move onto the next set of array lessons.
+
+// Remove all of your code from the previous step.
